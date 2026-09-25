858074a8e30bbae2a114f03cb7c38ff706c933bf
Александр <shura.yablonskij@mail.ru>
2026-09-25 09:56:31 +0300

    Созданы модели и настроена админка: Recipient, Message, Mailing, Attempt

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Recipient, Message, Mailing, Attempt
 
-# Register your models here.
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'comment')
+    search_fields = ('email', 'full_name')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject',)
+    search_fields = ('subject',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'start_time', 'end_time', 'status')
+    list_filter = ('status',)
+    filter_horizontal = ('recipients',)
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('id', 'mailing', 'attempt_time', 'status')
+    list_filter = ('status',)
